7d3f54a5ed3d5859ff1d0465767b91c87e499e26
chang <changdong1687@gmail.com>
2026-06-10 02:09:25 +0000

    add test_train_wan22.sh

---
+++ test_train_wan22.sh
@@ -0,0 +1,16 @@
+  export DREAMZERO_ROOT=/inspire/hdd/project/realtimedecisionmaking/chentao-25011/surd/codes/ccc_rrrr
+  export LIBERO_DATA_ROOT=/inspire/hdd/project/realtimedecisionmaking/chentao-25011/surd/codes/ccc_rrrr/data/libero_spatial
+
+  export WAN22_CKPT_DIR=/inspire/hdd/project/realtimedecisionmaking/chentao-25011/surd/codes/ccc_rrrr/checkpoints/Wan2.2-TI2V-5B
+  export IMAGE_ENCODER_DIR=/inspire/hdd/project/realtimedecisionmaking/chentao-25011/surd/codes/ccc_rrrr/checkpoints/Wan2.1-I2V-14B-480P
+  export TOKENIZER_DIR=/inspire/hdd/project/realtimedecisionmaking/chentao-25011/surd/codes/ccc_rrrr/checkpoints/umt5-xxl
+
+  export OUTPUT_DIR=/inspire/hdd/project/realtimedecisionmaking/chentao-25011/surd/codes/ccc_rrrr/checkpoints/dreamzero_libero_wan22_lora_test
+
+  export NUM_GPUS=1
+  export PER_DEVICE_BS=1
+  export GRADIENT_ACCUM=1
+  export MAX_STEPS=1000
+  export WANDB_MODE=offline
+
+  bash simulation/libero/train_libero_wan22.sh
